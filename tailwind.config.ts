ac35ecde68bd629acec633299b89cebd9970788a
moonformeli <mgyang95@gmail.com>
2024-12-04 10:35:32 +0900

    [TRIBE-113] chore: 테일윈드에 색상 추가

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{ts,tsx}", "./.storybook/**/*.{js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "tribe-green": "#41DEAC",
+        "tribe-orange": "#F8680E",
+        "tribe-navy": "#07143A",
+        "tribe-informative": "#2B87EB",
+        "tribe-error": "#DE3535",
+      },
+    },
+  },
+  plugins: [],
+} as Config;
